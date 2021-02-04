81e76c3ef9e6757fe139751fce61d0651aea59d0
mvakkasoglu <merve.vakkasoglu@gmail.com>
2021-02-04 01:00:04 -0500

    Jenkinsfile modified

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,21 +9,21 @@ pipeline {
 		stage('Set up') {
 			steps {
 				script {
-					sh 'rm -rf  2020_03_DO_Boston_casestudy_part_1'
+					sh 'rm -rf  capstone-project'
 				}
 			}
 		}
 		stage('SCM Checkout') {
 			steps {
 				script {
-					sh 'git clone https://github.com/mvakkasoglu/2020_03_DO_Boston_casestudy_part_1.git'
+					sh 'git clone https://github.com/mvakkasoglu/capstone-project.git'
 				}
 			}
 		}
 		stage('Build') {
 			steps {
 				script {
-				    dir('./2020_03_DO_Boston_casestudy_part_1') {
+				    dir('./capstone-project') {
 				        sh 'pip install -r requirements.txt'
 				    }
 			    }
